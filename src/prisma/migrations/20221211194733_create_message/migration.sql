1e684aac9a87f2d1ad851cfc23a199a4249390bc
Johan Holmberg <johan@kaxig.com>
2022-12-11 23:41:29 +0100

    Prep for auth

--- src/prisma/migrations/20221211194733_create_message/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `Conversation` ADD COLUMN `deleted` BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE `User` ADD COLUMN `deleted` BOOLEAN NOT NULL DEFAULT false;
--- src/prisma/migrations/20221211223250_init/migration.sql
+++ src/prisma/migrations/20221211223250_init/migration.sql
@@ -1,8 +1,25 @@
 -- CreateTable
+CREATE TABLE `RefreshToken` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `createdByIp` VARCHAR(191) NOT NULL,
+    `expiresAt` DATETIME(3) NOT NULL,
+    `replacedByToken` VARCHAR(191) NOT NULL,
+    `revokedAt` DATETIME(3) NOT NULL,
+    `revokedByIp` VARCHAR(191) NOT NULL,
+    `reasonRevoked` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `User` (
     `id` VARCHAR(191) NOT NULL,
     `username` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
+    `deleted` BOOLEAN NOT NULL DEFAULT false,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NULL,
 
@@ -13,6 +30,7 @@ CREATE TABLE `User` (
 CREATE TABLE `Conversation` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
+    `deleted` BOOLEAN NOT NULL DEFAULT false,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NULL,
 
@@ -66,11 +84,3 @@ ALTER TABLE `Message` ADD CONSTRAINT `Message_conversationId_fkey` FOREIGN KEY (
 
 -- AddForeignKey
 ALTER TABLE `Message` ADD CONSTRAINT `Message_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- Add some initial data
-INSERT INTO ConversationUserRole (id, name)
-VALUES 
-('SUPERUSER', 'Superuser'),
-('ADMIN', 'Admin'),
-('MODERATOR', 'Moderator'),
-('MEMBER', 'Member');
